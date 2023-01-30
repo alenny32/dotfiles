ce9e4d039a34fad6ca8f4eff698d44c5ac64d5c0
Umar Ibrahim <umaribrahim435@gmail.com>
2023-01-30 11:22:09 +0700

    add pipewire stuff to xinitrc
    add install pipewire script to switch from pulseaudio to pipewire
    fix dmenu prompt script not working

---
+++ scripts/sys/install-pipewire.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Script to switch from pulseaudio to pipewire
+
+# Remove pulseaudio and all packages depends on it first, then
+# install pipewire and all of uninstalled packages
+
+## Elevating privilege
+# chusr="sudo"
+chusr="doas"
+
+${chusr} pacman -Rns --noconfirm \
+    pulseaudio-alsa \
+    pulseaudio-bluetooth \
+    pulseaudio-jack \
+    pulsemixer \
+    pulseaudio-equalizer \
+    pulseaudio-equalizer-ladspa \
+    pulseaudio \
+    gnome-bluetooth \
+    blueberry \
+    blueman \
+    volumeicon \
+    pavucontrol
+
+${chusr} pacman -S --needed \
+    pipewire \
+    pipewire-media-session \
+    pipewire-alsa \
+    pipewire-zeroconf \
+    pipewire-pulse \
+    blueman \
+    volumeicon \
+    pavucontrol
+
+    # pipewire-jack \ # not needed if jack (jack2, jack2-dbus) is installed
+
+echo "Reboot now"
