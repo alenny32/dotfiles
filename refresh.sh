dcb804ff2841d5870badb794ac35c10b7ac4795d
Umar Ibrahim <umaribrahim435@gmail.com>
2023-02-02 00:56:19 +0700

    remove guake config and unused old script

--- refresh.sh
+++
@@ -1,8 +0,0 @@
-# combine scripts
-# copy-config.sh then stow-compat.sh
-
-./copy-config.sh
-./stow-compat.sh
-
-# Update git submodule
-echo "\033[38;5;196;48;5;231m Update git submodule!! \n Use: git submodule update --init \033[0m"
